4334155e23e583fd74894ac0179d9624c5806c39
Thulinma <jaron@vietors.com>
2016-03-01 21:36:58 +0100

    Updated count_talks.sh script to check for existance of encoded files and warn if not there

--- count_talks.sh
+++ count_talks.sh
@@ -11,16 +11,26 @@ for i in `grep -l "ROOM='${ROOM}'" ~/videobox/schedule/*`; do
 
   TITLE=$(grep TITLE $i);
 
-  if grep WEBMONLY $i > /dev/null; then echo "OK: ${TITLE:6}"; continue; fi
-  
+  OUT_DIR='/home/fosdem/storage/rsync_to_video_fosdem_org'
+  OUT_FILE=$(echo ${TITLE:6} | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+  FILENAME="${OUT_DIR}/${ROOM}/${OUT_FILE}.mp4"
+
+  if [ -e $FILENAME ] ; then
+    RDY=""
+  else
+    RDY="NOT AVAILABLE YET - WILL BE FIXED ASAP (`basename $i`) - "
+  fi
+
+  if grep WEBMONLY $i > /dev/null; then echo "OK: ${RDY}${TITLE:6}"; continue; fi
+
   if grep NOAUDIO $i > /dev/null; then echo "LOST: (no audio) ${TITLE:6}"; continue; fi
   if grep LOST $i > /dev/null; then echo "LOST: ${TITLE:6}"; continue; fi
-  if grep NOSLIDES $i > /dev/null; then echo "PARTIAL: (no slides) ${TITLE:6}"; continue; fi
-  if grep "NO SLIDES" $i > /dev/null; then echo "PARTIAL: (no slides) ${TITLE:6}"; continue; fi
-  if grep PARTIAL $i > /dev/null; then echo "PARTIAL: ${TITLE:6}"; continue; fi
+  if grep NOSLIDES $i > /dev/null; then echo "PARTIAL: ${RDY}(no slides) ${TITLE:6}"; continue; fi
+  if grep "NO SLIDES" $i > /dev/null; then echo "PARTIAL: ${RDY}(no slides) ${TITLE:6}"; continue; fi
+  if grep PARTIAL $i > /dev/null; then echo "PARTIAL: ${RDY}${TITLE:6}"; continue; fi
 
   if grep -E '^CAM_END=$' $i >/dev/null; then echo "UNKNOWN: ${TITLE:6}"; continue; fi;
-    
-  echo "OK: ${TITLE:6}"
+
+  echo "OK: ${RDY}${TITLE:6}"
 done
 
